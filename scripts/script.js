25175fa61173f6a027f9810c464b3ee800b04d38
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-18 15:24:02 +0300

    Implement function to create columns

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,11 @@ const container = document.querySelector("#container");
 container.style.display = "flex";
 
 let grid = [];
+
+function createColumn() {
+    let column = document.createElement("div");
+    column.style.display = "flex";
+    column.style.flexDirection = "column";
+
+    return column;
+}
